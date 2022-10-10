ad6e96d4ba25f35d685aa0fe1082c93e61366037
Gabriel Maires <gabriel.maires12@gmail.com>
2022-10-09 22:56:39 -0300

    models e admin iniciais

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Motorista(models.Model):
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=30)
+    email = models.EmailField()
+    celular = models.CharField(max_length=11)
+    codigo = models.CharField(max_length=8)
+    prod_entregar = models.IntegerField()
+    prod_falha = models.IntegerField()
+    prod_em_curso = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=30)
+    codigo = models.CharField(max_length=8)
+    endereco_entrega = models.TextField(max_length=250)
+    proprietario = models.CharField(max_length=100)
+    celular1 = models.CharField(max_length=11)
+    celular2 = models.CharField(max_length=11)
+    localizacao = models.CharField(max_length=30)
--- storage/views.py
+++ storage/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Motorista, Produto
+
 
 def storage_page(request):
-    return render(request, 'storage_homepage.html')
+    motorista = Motorista.objects.all()
+    produto = Produto.objects.all()
+    return render(request, 'storage_homepage.html', {"motoristas": motorista, "produtos": produto})
